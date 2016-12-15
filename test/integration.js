1156f3b1df71a98deac10742a3f8d32e320edfb3
Tom Alcorn <tdb.alcorn@gmail.com>
2016-12-15 13:44:25 -0800

    Remove test index.html and make the integration test recreate it on the fly.

--- test/integration.js
+++ test/integration.js
@@ -1,5 +1,6 @@
 var assert = require("assert");
 var http = require("http");
+var fs = require("fs");
 var Router = require("../router.js");
 var open = require("nice-open");
 
@@ -11,7 +12,7 @@ describe("Router", function() {
     });
     it("should show index.html by default", function() {
         return new Promise(function(resolve, reject) {
-            // TODO make test create index.html and then delete it after its done
+            fs.writeFileSync("index.html", "<html><body><p>Success!</p></body></html>");
             var router = new Router();
             var url = "http://localhost:" + port;
             router.listen(port);
@@ -19,7 +20,10 @@ describe("Router", function() {
                 assert.equal(res.statusCode, 200);
             });
             open(url);
-            setTimeout(function() { resolve(router); }, 1000);
+            setTimeout(function() {
+                fs.unlinkSync("index.html");
+                resolve(router);
+            }, 1000);
         }).then(function(router) { router.close(); });
     }).timeout(10000);
     it("should show an error page when the handler fails", function() {
@@ -47,7 +51,7 @@ describe("Router", function() {
                 assert.equal(res.statusCode, 200);
             });
             open(url);
-            setTimeout(done, 9000);
+            setTimeout(done, 1000);
         }).timeout(10000);
     });
 });
